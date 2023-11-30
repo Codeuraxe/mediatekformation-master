816416302e9592a5195fa0eb2b8242fa0fddfe3a
linux <fire.bilix@gmail.com>
2023-11-30 17:20:49 +0100

    Tâche 1 : done

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Repository\CategorieRepository;
@@ -8,32 +9,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Controleur des formations
- *
- * @author emds
- */
 class FormationsController extends AbstractController {
-    
-    const PAGES_PATH = "pages/formations.html.twig";
 
-    /**
-     * 
-     * @var FormationRepository
-     */
     private $formationRepository;
-    
-    /**
-     * 
-     * @var CategorieRepository
-     */
     private $categorieRepository;
-    
-    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+
+    private const FORMATIONS_PATH = 'pages/formations.html.twig';
+    private const FORMATION_PATH = 'pages/formation.html.twig';
+
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
         $this->formationRepository = $formationRepository;
-        $this->categorieRepository= $categorieRepository;
+        $this->categorieRepository = $categorieRepository;
     }
-    
+
     /**
      * @Route("/formations", name="formations")
      * @return Response
@@ -41,7 +29,9 @@ class FormationsController extends AbstractController {
     public function index(): Response{
         $formations = $this->formationRepository->findAll();
         $categories = $this->categorieRepository->findAll();
-        return $this->render(PAGES_PATH, [
+
+       
+        return $this->render(self::FORMATIONS_PATH, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -49,49 +39,54 @@ class FormationsController extends AbstractController {
 
     /**
      * @Route("/formations/tri/{champ}/{ordre}/{table}", name="formations.sort")
-     * @param type $champ
-     * @param type $ordre
-     * @param type $table
+     * @param string $champ
+     * @param string $ordre
+     * @param string $table
      * @return Response
      */
-    public function sort($champ, $ordre, $table=""): Response{
+    public function sort($champ, $ordre, $table = ""): Response{
         $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render(self::PAGES_PATH, [
+
+       
+        return $this->render(self::FORMATIONS_PATH, [
             'formations' => $formations,
             'categories' => $categories
         ]);
-    }     
-    
+    }
+
     /**
      * @Route("/formations/recherche/{champ}/{table}", name="formations.findallcontain")
-     * @param type $champ
+     * @param string $champ
      * @param Request $request
-     * @param type $table
+     * @param string $table
      * @return Response
      */
-    public function findAllContain($champ, Request $request, $table=""): Response{
+    public function findAllContain($champ, Request $request, $table = ""): Response{
         $valeur = $request->get("recherche");
         $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render(self::PAGES_PATH, [
+
+       
+        return $this->render(self::FORMATIONS_PATH, [
             'formations' => $formations,
             'categories' => $categories,
             'valeur' => $valeur,
             'table' => $table
         ]);
     }  
-    
+
     /**
      * @Route("/formations/formation/{id}", name="formations.showone")
-     * @param type $id
+     * @param int $id
      * @return Response
      */
     public function showOne($id): Response{
         $formation = $this->formationRepository->find($id);
-        return $this->render(self::PAGES_PATH, [
+
+        
+        return $this->render(self::FORMATION_PATH, [
             'formation' => $formation
         ]);        
-    }   
-    
+    }
 }
